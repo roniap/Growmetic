2719d8b8b01e0f5cd712ea879d7184ddba4029d1
zsociety <autoponik@gmail.com>
2019-09-19 09:21:33 +0700

    change lib C++ compotible with gcc>5

--- Growmetic.ino
+++ Growmetic.ino
@@ -3,6 +3,9 @@
 *  DROMatic OS Core
 *  Devin R. Olsen - July 4th, 2017
 *  devin@devinrolsen.com
+*  Derivative from Olsen DROmatic
+*  Change LCD lib to I2C
+*  Change C++ lib competible g++ >5
 */
 #include <Wire.h>    //Include the software serial library for white sheild
 #include <Adafruit_NeoPixel.h>
@@ -35,6 +38,11 @@ void setup()
 	lcd.begin();
 	// Turn on the blacklight and print a message.
 	lcd.backlight();
+  lcd.print(F("....STARTING.... "));
+  lcd.setCursor(0, 1);
+  lcd.print(F("..GROWMETIC OS.."));
+  delay(3000);
+  lcd.clear();
 	//lcd.begin(16, 2);
 	pixels.begin(); // This initializes the NeoPixel library.
 	pixels.setBrightness(128);
